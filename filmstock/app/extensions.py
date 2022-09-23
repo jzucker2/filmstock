c896ce21026d59dcd421a1cb2d702b230ac764ee
Jordan Zucker <jordanz@pubnub.com>
2022-09-23 14:19:43 -0700

    Fix the apscheduler reference. Dropped for now

--- filmstock/app/extensions.py
+++ filmstock/app/extensions.py
@@ -1,8 +1,6 @@
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_apscheduler import APScheduler
 
 
-scheduler = APScheduler()
 migrate = Migrate()
 cors = CORS  # not instantiated so we can use it in `__init__.py`
