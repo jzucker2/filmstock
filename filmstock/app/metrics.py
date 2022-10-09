ea95bd4364dad9a95d478fe53d4f60f5bf3d8a41
Jordan Zucker <jordanz@pubnub.com>
2022-10-09 16:23:03 -0700

    I'm doing this in a few places now. So write it down

--- filmstock/app/metrics.py
+++ filmstock/app/metrics.py
@@ -1,4 +1,31 @@
+from enum import Enum
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+from prometheus_flask_exporter import Counter, Summary, Gauge
+
+
+class Labels(Enum):
+    ACTIVITY_NAME = 'activity_name'
+
+    @classmethod
+    def labels(cls):
+        return list([
+            cls.ACTIVITY_NAME.value,
+        ])
+
+
+EXAMPLE_TIME = Summary(
+    'example_time_seconds',
+    'Time spent on example function')
+
+
+EXAMPLE_EXCEPTIONS_COUNTER = Counter(
+    'example_exceptions',
+    'Exceptions during example function')
+
+
+EXAMPLE_GAUGE = Gauge(
+    'example_gauge',
+    'Example gauge during function')
 
 
 # https://github.com/rycus86/prometheus_flask_exporter#app-factory-pattern
